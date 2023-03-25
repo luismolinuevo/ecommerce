bf65a7c4b771e0878261ac8a070881fcf9748486
luismolinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-03-25 13:48:27 -0400

    set up user model

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config() 
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
+
+const db = process.env.DATABASE_URL;
 
 export default function createServer() {
+    //connects mongoose to mongodb database
+    mongoose.set("strictQuery", false);
+    mongoose.set('bufferCommands', false);
+
+    mongoose.connect(db);
+    mongoose.connection.on('error', (err) => {
+        console.log(err);
+    });
     const app = express();
     app.use(cors())
     app.use(express.json());
---
+++ server/models/product.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const products = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 4,
+        maxLength: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 16
+    }
+})
+
+
+module.exports = mongoose.model("Product", products)
